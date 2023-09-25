1faf3ba145984b5c7795f3cc87404e78cdfc2bfb
ccarissac <112513067+ccarissac@users.noreply.github.com>
2023-09-25 04:03:59 -0400

    calculations

--- solution-hw3/js/app.js
+++ solution-hw3/js/app.js
@@ -25,9 +25,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let selectGlaze = document.getElementById("glazingOptions");
     for (var j = 0; j < bunGlaze.length; j++) {
         var bunOption = document.createElement("option");
-    bunOption.text=bunGlaze[j].option;
-    bunOption.value=bunGlaze[j].price;
-    selectGlaze.add(bunOption);
+        bunOption.text=bunGlaze[j].option;
+        bunOption.value=bunGlaze[j].price;
+        selectGlaze.add(bunOption);
     }
 });
 
@@ -56,14 +56,43 @@ document.addEventListener('DOMContentLoaded', function() {
     let selectCount = document.getElementById("countOptions");
     for (var j = 0; j < allCount.length; j++) {
         var countOption = document.createElement("option");
-    countOption.text=allCount[j].size;
-    countOption.value=allCount[j].adapt;
-    selectCount.add(countOption);
+        countOption.text=allCount[j].size;
+        countOption.value=allCount[j].adapt;
+        selectCount.add(countOption);
     }
 });
 
-function glazingChange(element) {
+//Initial values
+let basePrice=2.49;
+let bunPrice= 0;
+let bunCount= 1;
+document.getElementById("glazingOptions").addEventListener("change", newPrice);
+document.getElementById("countOptions").addEventListener("change", newPrice);
 
-    const priceChange =element.value;
-    (basePrice+glazingPrice)*packPrice;
+function glazingChange(){
+    let bunPrice= document.getElementById("glazingOptions").value;
+    console.log (bunPrice);
 }
+
+function countChange(){
+    let bunCount= document.getElementById("countOptions").value;
+    console.log (bunCount);
+    // document.getElementById("countOptions").addEventListener("DOMContentLoaded", newPrice);
+    }
+    // 
+
+// function countChange() {
+//     let bunCount= document.getElementById("countOptions").value;
+//     console.log(bunCount);
+//     document.getElementById("countOptions").addEventListener("change", newPrice);
+//     }
+
+
+function newPrice() {
+    calcPrice=(basePrice+bunPrice)*bunCount;
+    console.log(calcPrice);
+    document.getElementById("detail-price").innerHTML = "$" +calcPrice.toFixed(2);
+}
+
+    // const priceChange =element.value;
+    // (basePrice+glazingPrice)*packPrice;
